90192389dba571f36effc9fb4b19732688ca71f0
vr256 <volodyarizun4@gmail.com>
2023-08-12 06:18:28 +0300

    fix warnings

--- __main__.py
+++ __main__.py
@@ -1,14 +1,16 @@
 import os
 import sys
+import warnings
 
 ROOT = os.path.abspath(os.path.join(__file__, ".."))
 sys.path.insert(1, ROOT)
-
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 from tools.config import LOGO_PATH
 from views import App
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
     app = App()
     app.iconbitmap(LOGO_PATH)
     app.mainloop()
